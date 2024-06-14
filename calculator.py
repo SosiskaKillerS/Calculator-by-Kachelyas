fb953a688c953319972e62bb3d3cf927265ca71d
nick/ga <158477658+SosiskaKillerS@users.noreply.github.com>
2024-06-14 16:25:42 +0300

    Update buttons

--- calculator.py
+++ calculator.py
@@ -1,22 +1,33 @@
 import tkinter as tk
+
+
+def add_digit(digit):
+    value = calc.get() + str(digit)
+    calc.delete(0, tk.END)
+    calc.insert(0,value)
+
+def make_digit_button(digit):
+    return tk.Button(text=digit, bd=5, font=('Arial',13), command=lambda : add_digit(digit))
+
+
 win = tk.Tk()
 win.geometry(f"240x260+100+200")
 win['bg'] = '#33ffe6'
 win.title('Calculator')
 
-calc = tk.Entry(win)
-calc.grid(row=0,column=0,columnspan=3)
+calc = tk.Entry(win, justify=tk.RIGHT, font = ('Arial', 15), width = 15)
+calc.grid(row=0,column=0,columnspan=3, stick='we')
 
-tk.Button(text='1').grid(row=1,column=0, stick='wens', pady=5, padx=5)
-tk.Button(text='2').grid(row=1,column=1, stick='wens', pady=5, padx=5)
-tk.Button(text='3').grid(row=1,column=2, stick='wens', pady=5, padx=5)
-tk.Button(text='4').grid(row=2,column=0, stick='wens', pady=5, padx=5)
-tk.Button(text='5').grid(row=2,column=1, stick='wens', pady=5, padx=5)
-tk.Button(text='6').grid(row=2,column=2, stick='wens', pady=5, padx=5)
-tk.Button(text='7').grid(row=3,column=0, stick='wens', pady=5, padx=5)
-tk.Button(text='8').grid(row=3,column=1, stick='wens', pady=5, padx=5)
-tk.Button(text='9').grid(row=3,column=2, stick='wens', pady=5, padx=5)
-tk.Button(text='0').grid(row=4,column=0, stick='wens', pady=5, padx=5)
+make_digit_button('1').grid(row=1,column=0, stick='wens', pady=5, padx=5)
+make_digit_button('2').grid(row=1,column=1, stick='wens', pady=5, padx=5)
+make_digit_button('3').grid(row=1,column=2, stick='wens', pady=5, padx=5)
+make_digit_button('4').grid(row=2,column=0, stick='wens', pady=5, padx=5)
+make_digit_button('5').grid(row=2,column=1, stick='wens', pady=5, padx=5)
+make_digit_button('6').grid(row=2,column=2, stick='wens', pady=5, padx=5)
+make_digit_button('7').grid(row=3,column=0, stick='wens', pady=5, padx=5)
+make_digit_button('8').grid(row=3,column=1, stick='wens', pady=5, padx=5)
+make_digit_button('9').grid(row=3,column=2, stick='wens', pady=5, padx=5)
+make_digit_button('0').grid(row=4,column=0, stick='wens', pady=5, padx=5)
 
 win.grid_columnconfigure(0,minsize=60)
 win.grid_columnconfigure(1,minsize=60)
